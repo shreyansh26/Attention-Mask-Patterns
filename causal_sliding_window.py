3d2931610650641dc14ad5abb4b0aacb96058839
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2024-09-09 02:39:27 -0700

    Update plots

--- causal_sliding_window.py
+++ causal_sliding_window.py
@@ -69,4 +69,4 @@ visualize_attention_scores(q, k, mask_mod=mask_mod, name=name, path=Path(f"plots
 plot_bar_graph([flex_attention_fwd_time, flex_attention_bwd_time, "FlexAttention"], 
                 [xformers_sdpa_with_mask_fwd_time, xformers_sdpa_with_mask_bwd_time, "xFormers/SDPA + mask"], 
                 [fa_fwd_time, fa_bwd_time, "FA (Causal)"], 
-                name="timing", path=Path(f"plots/{name}/timing.png"))
+                name=name, path=Path(f"plots/{name}/timing.png"))
